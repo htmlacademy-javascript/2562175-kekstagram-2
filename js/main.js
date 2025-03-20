13b16be705601acacfe4095917f0c06fe70c9685
OLGAPOPKOVA210470 <popkovaolga1970@gmail.com>
2025-03-20 23:36:57 +0300

    выполнение задания 4.16

--- js/main.js
+++ js/main.js
@@ -16,23 +16,91 @@ const MESSAGES = [
 ];
 
 const DESCRIPTIONS = [
-'Солнечный денёк.', 'Дорога к пляжу.', 'Шум прибоя.', 'Вот она-свобода!', 'Отдыхаем.', 'Черная молния.', 'На диете.', 
-'Заряд бодрости.', 'Первым делом-самолёты.', 'Разложить всё по полочкам.', 'Прямо пойдешь-к морюшку придёшь.', 
-'Зеленоглазое такси.', 'Радуга в тарелке.', 'Кексоролл.', 'Луностопы.', 'В погоне за мечтой.',
- 'Вместе весело шагать по просторам и, конечно, припевать лучше хором.', 'Без комментариев.', 'Волшебные калоши.', 'Пальма финиковая - 8 штук.',
-  'Все на ужин!', 'Как Магдалена, морская пена.', 'Я тебя съем.', 'Фаер-шоу в Турции.', 'Оставьте бегемота в покое!' 
+    'Солнечный денёк.', 'Дорога к пляжу.', 'Шум прибоя.', 'Вот она-свобода!', 'Отдыхаем.', 'Черная молния.', 'На диете.',
+    'Заряд бодрости.', 'Первым делом-самолёты.', 'Разложить всё по полочкам.', 'Прямо пойдешь-к морюшку придёшь.',
+    'Зеленоглазое такси.', 'Радуга в тарелке.', 'Кексоролл.', 'Луностопы.', 'В погоне за мечтой.',
+    'Вместе весело шагать по просторам и, конечно, припевать лучше хором.', 'Без комментариев.', 'Волшебные калоши.', 'Пальма финиковая - 8 штук.',
+    'Все на ужин!', 'Как Магдалена, морская пена.', 'Я тебя съем.', 'Фаер-шоу в Турции.', 'Оставьте бегемота в покое!'
 ];
 
 const PHOTOS_COUNT = 25;
 
-const LAKES_MINIMUM_QUANTITY = 15;
+const DESCRIPTIONS_COUNT = 25;
 
-const LAKES_MAXIMUM_QUANTITY = 200;
+const MIN_LAKES = 15;
 
-const COMMENTS_MINIMUM_QUANTITY = 0;
+const MAX_LAKES = 200;
 
-const COMMENTS_MAXIMUM_QUANTITY = 30;
+const MIN_COMMENTS = 0;
 
-const AVATARS_MINIMUM_QUANTITY = 1;
+const MAX_COMMENTS = 30;
 
-const AVATARS_MAXIMUM_QUANTITY = 6;
+const MIN_AVATARS = 1;
+
+const MAX_AVATARS = 6;
+
+//Функция получения случайного числа из заданного диапазона
+
+const getRandomInteger = (a, b) => {
+    const lower = Math.ceil(Math.min(a, b));
+    const upper = Math.floor(Math.max(a, b));
+    let previousResult = -1;
+    return () => {
+        const result = Math.floor(Math.random() * (upper - lower + 1) + lower);
+        if (previousResult !== result) {
+            previousResult = result;
+            return result;
+        };
+        return result === upper ? lower : result + 1;
+    };
+};
+
+//Вложенный объект Comments
+
+const createComment = () => {
+    let id = 1;
+    const indexMessageArr = getRandomInteger(0, MESSAGES.length - 1);
+    const indexNameArr = getRandomInteger(0, NAMES.length - 1);
+    //Возвращаемая функция
+    return () => {
+        const comment = {};
+        const idAvatar = getRandomInteger(MIN_AVATARS, MAX_AVATARS);
+        comment.id = id;
+        comment.avatar = `img/avatar-${idAvatar()}.svg`;
+        comment.message = `${MESSAGES[indexMessageArr()]}. ${MESSAGES[indexMessageArr()]}`;
+        comment.name = `${NAMES[indexNameArr()]}`;
+        id++;
+        return comment;
+    };
+};
+
+//Количество комментариев
+
+const numComments = getRandomInteger(MIN_COMMENTS, MAX_COMMENTS);
+
+//Количество лайков
+
+const numLakes = getRandomInteger(MIN_LAKES, MAX_LAKES);
+
+//функция создания объекта
+
+const createPhoto = () => {
+    let id = 1;
+    return () => {
+        const photo = {};
+        photo.id = id;
+        photo.url = `photos/${id}.jpg`;
+        //Массив описаний
+        photo.descriptions = Array.from({ length: DESCRIPTIONS_COUNT }, createComment());
+        photo.lakes = numLakes();
+        //Массив комментариев
+        photo.comments = Array.from({ length: numComments() }, createComment());
+        id++;
+        return photo;
+    };
+};
+//Массив фотографий
+
+const photoArray = Array.from({ length: PHOTOS_COUNT }, createComment());
+
+console.log(photoArray);
