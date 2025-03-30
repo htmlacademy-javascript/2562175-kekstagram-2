960f194b8cea55d3efb5a035474c2e244f1bfd74
OLGAPOPKOVA210470 <popkovaolga1970@gmail.com>
2025-03-30 22:20:10 +0300

    тренировка на кубиках

--- js/data.js
+++ js/data.js
@@ -1,39 +1,5 @@
-import { getRandomInteger } from './util.js';
-import { getUnique } from './util.js';
-import {getRandomElement } from './util.js';
-
-const NAMES = [
-    'Виктория', 'Дарья', 'Алексей', 'Александра', 'Сергей', 'Ульяна', 'Патрик', 'Александр', 'Марина', 'Станислав',
-    'Елизавета', 'Валентина', 'Егор', 'Фёдор', 'Ольга', 'Владимир', 'Анна', 'Людмила', 'Зоя', 'Виктор',
-    'Анатолий', 'Вадим', 'Светлана', 'Екатерина', 'Мария', 'Михаил', 'Иван', 'Илья', 'Дмитрий', 'Маргарита'
-];
-
-const MESSAGES = [
-    'Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
-
-const DESCRIPTIONS = [
-    'Санаторий "Железнодорожник".', 'Солнечный денёк.', 'Дорога к пляжу.', 'Шум прибоя.', 'Вот она-свобода!', 'Отдыхаем.', 'Черная молния.', 'На диете.',
-    'Заряд бодрости.', 'Первым делом-самолёты.', 'Разложить всё по полочкам.', 'Прямо пойдешь-к морюшку придёшь.',
-    'Зеленоглазое такси.', 'Радуга в тарелке.', 'Кексоролл.', 'Луностопы.', 'В погоне за мечтой.',
-    'Вместе весело шагать по просторам и, конечно, припевать лучше хором.', 'Без комментариев.', 'Волшебные калоши.', 'Пальма финиковая - 8 штук.',
-    'Все на ужин!', 'Как Магдалена, морская пена.', 'Я тебя съем.', 'Фаер-шоу в Турции.', 'Оставьте бегемота в покое!'
-];
-
-const MIN_PHOTOS = 1;
-const MAX_PHOTOS = 25;
-const DESCRIPTIONS_COUNT = 25;
-const MIN_LAKES = 15;
-const MAX_LAKES = 200;
-const MIN_COMMENTS = 0;
-const MAX_COMMENTS = 30;
-const MIN_AVATARS = 1;
-const MAX_AVATARS = 6;
+import { getRandomInteger,getUnique,getRandomElement } from './util.js';
+import {NAMES, MESSAGES, DESCRIPTIONS, MIN_PHOTOS, MAX_PHOTOS, DESCRIPTIONS_COUNT, MIN_LAKES, MAX_LAKES, MIN_COMMENTS, MAX_COMMENTS, MIN_AVATARS, MAX_AVATARS} from "./constants.js"
 
 const getUniqueId = getUnique(MIN_PHOTOS, MAX_PHOTOS);
 const getUniquePhoto = getUnique(MIN_PHOTOS, MAX_PHOTOS);
@@ -77,6 +43,4 @@ const getPhoto = () => ({
     return result;
 };*/
 
-const createPhotos = () => Array.from({ length: MAX_PHOTOS }, getPhoto);
-
-console.log(createPhotos())
+export const createPhotos = () => Array.from({ length: MAX_PHOTOS }, getPhoto);
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
-import './functions.js';
-import './data.js';
-import './util.js';
+
+import {createPhotos} from "./data.js";
+
+console.log(createPhotos())
