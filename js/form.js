638c77d4d9b70ce73a308216d46258999962c793
OLGAPOPKOVA210470 <popkovaolga1970@gmail.com>
2025-04-27 18:32:55 +0300

    подключение escape

--- js/form.js
+++ js/form.js
@@ -1,6 +1,7 @@
 import { postData } from "./api.js";
 import { Popups } from "./constants.js";
 import { reset as resetEffect } from "./effects.js";
+import { removeEscapeControl, setEscapeControl } from "./escapeControl.js";
 import { showPopup } from "./popup.js";
 import { reset as resetScale } from "./scale.js";
 import { isValid } from "./validation.js";
@@ -9,16 +10,22 @@ const body = document.body;
 const photoUploadForm = document.querySelector('.img-upload__form');
 const uploadFileControl = photoUploadForm.querySelector('#upload-file');
 const descriptionField = photoUploadForm.querySelector('.text__description');
+const hashtagTag = photoUploadForm.querySelector('.text__hashtags');
 const photoUploadBtn = photoUploadForm.querySelector('.img-upload__input');
 const photoEditModal = photoUploadForm.querySelector('.img-upload__overlay');
 const photoBigPreview = photoUploadForm.querySelector('.img-upload__preview img');
 const photoSmallPreviews = document.querySelectorAll('.effects__preview');
 const closeButton = photoUploadForm.querySelector('.img-upload__cancel');
 
+const canCloseForm = () => !(document.activeElement === descriptionField || document.activeElement === hashtagTag);
+
 const openForm = () => {
+  // document.addEventListener('DOMContentLoaded', () => {
+  //  photoUploadBtn.addEventListener('input', () => {
   photoEditModal.classList.remove('hidden');
   body.classList.add('modal-open');
   const file = photoUploadBtn.files[0];
+  setEscapeControl(closeForm, canCloseForm);
 
   if (file && file.type.startsWith('image/')) {
     const imageURL = URL.createObjectURL(file);
@@ -29,32 +36,21 @@ const openForm = () => {
   } else {
     console.warn('Файл не является изображением или не выбран');
   }
+  //});
+  // });
 };
 
-const closeForm = () => {
+function closeForm() {
   photoEditModal.classList.add('hidden');
   body.classList.remove('modal-open');
-  //  photoUploadBtn.value = '';
-  //photoBigPreview.src = '';
-  // photoSmallPreviews.forEach((photoSmallPreview) => {
-  //    photoSmallPreview.style.backgroundImage = '';
-  // });
-
   photoUploadForm.reset();
   resetScale();
-  resetEffect()
+  resetEffect();
 };
 
 closeButton.addEventListener('click', () => {
-  closeForm()
-});
-
-document.addEventListener('keydown', (evt) => {
-  if (evt.key === 'Escape') {
-    evt.preventDefault();
-    photoEditModal.classList.add('hidden');
-    body.classList.remove('modal-open');
-  };
+  closeForm();
+  removeEscapeControl()
 });
 
 photoUploadForm.addEventListener('submit', (evt) => {
@@ -65,28 +61,23 @@ photoUploadForm.addEventListener('submit', (evt) => {
     postData(new FormData(photoUploadForm))
       .then((response) => {
         if (!response.ok) {
-          throw new Error()
+          throw new Error();
         }
         closeForm();
+        removeEscapeControl();
         showPopup(Popups.SUCCESS);
       })
       .catch(() => {
         showPopup(Popups.ERROR);
       })
-      .finally(()=>{
+      .finally(() => {
         //разблокировать кнопку сабмита
-      })
-  }
-});
-
-descriptionField.addEventListener('keydown', (evt) => {
-  if (evt.key === 'Escape') {
-    evt.stopPropagation();
+      });
   }
 });
 
 uploadFileControl.addEventListener('change', () => {
   openForm();
-})
+});
 
 export { openForm, closeForm };
--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,6 @@
 import { Popups } from "./constants.js";
+import { removeEscapeControl, setEscapeControl } from "./escapeControl.js";
+
 const body = document.body;
 const successTemplate = document.querySelector('#success').content.querySelector('.success');
 const errorTemplate = document.querySelector('#error').content.querySelector('.error');
@@ -10,5 +12,15 @@ const templates = {
 
 export const showPopup = (type) => {
   const popup = templates[type].cloneNode(true);
+
+  popup.addEventListener('click', ({ target }) => {
+    if (target.classList.contains(type) || target.classList.contains(`${type}__button`)) {
+      popup.remove();
+      removeEscapeControl();
+    }
+  });
+  setEscapeControl(()=>{
+    popup.remove();
+  })
   body.append(popup)
-}
+};
